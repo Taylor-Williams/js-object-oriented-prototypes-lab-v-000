398ed704b34bbfa4a691a8b9b8d4c96f29dee23a
Taylor Williams <taylor@thewilliams.net>
2019-02-05 07:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function(){
   return 'No, I must disagree'
 }
-BoardMember.prototypeapprove = function(){
+BoardMember.prototype.approve = function(){
   return 'You can do that!'
 }
 BoardMember.prototype.doCharity = function(){
